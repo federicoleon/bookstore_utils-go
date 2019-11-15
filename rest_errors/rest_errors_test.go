f8b94f132ebfdd735a5a25999b261b109136a0c5
Federico León <fedeleon.cba@gmail.com>
2019-11-15 18:46:54 -0300

    Adding logger.

--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -10,13 +10,13 @@ import (
 func TestNewInternalServerError(t *testing.T) {
 	err := NewInternalServerError("this is the message", errors.New("database error"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "this is the message", err.Message)
-	assert.EqualValues(t, "internal_server_error", err.Error)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "this is the message", err.Message())
+	assert.EqualValues(t, "message: this is the message - status: 500 - error: internal_server_error - causes: [database error]", err.Error())
 
 	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes))
-	assert.EqualValues(t, "database error", err.Causes[0])
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "database error", err.Causes()[0])
 }
 
 func TestNewBadRequestError(t *testing.T) {
